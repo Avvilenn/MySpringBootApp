7a01b0661779de8a13ea7668d361ca7250a60981
Elena <avvilenn@gmail.com>
2017-05-31 16:20:43 +0300

    jsp problem taplib

--- src/main/java/com/mySpringBootApp/controller/IndexController.java
+++ src/main/java/com/mySpringBootApp/controller/IndexController.java
@@ -1,20 +1,31 @@
 package com.mySpringBootApp.controller;
 
+import com.mySpringBootApp.model.Person;
+import com.mySpringBootApp.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by просто on 31.05.2017.
  */
 @Controller
-public class IndexController implements ErrorController {
+public class IndexController implements ErrorController{
 
     private static final String PATH = "/error";
 
+    @Autowired
+    private PersonService service;
+
     @RequestMapping(value = PATH)
-    public String error() {
-        return "errorpage";
+    public String error(Model model) {
+        List<Person> personList = service.getAll();
+        model.addAttribute("PERSONS", personList);
+        return "welcome";
     }
 
     @Override
